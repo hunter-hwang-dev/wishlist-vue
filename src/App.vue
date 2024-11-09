87a14939528980960349a15284217a16e3e9f0bc
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-11-10 06:14:33 +0900

    design: figma 보고 하드코딩. AutoHTML의 필요성을 느끼다.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>
-    <h1>h1 글자 테스트</h1>
-    <h3>h3 글자 테스트</h3>
-    <p>프리텐다드</p>
+  <div class="wish-background">
+    <div style="padding: 14px">
+      <h3
+        class="fs-36"
+        style="font-weight: 500; text-align: left; line-height: 43px"
+      >
+        누군가가 이미<br />이 선물을 보냈어요!
+      </h3>
+      <hr class="present-state-line" />
+    </div>
+    <h3 class="fs-36" style="font-weight: 500; line-height: 36px">
+      글라스락 렌지쿡
+    </h3>
+    <span class="fs-20" style="font-weight: 500; line-height: 24px"
+      >탕, 찌개용</span
+    >
+    <img src="./assets/test.png" />
+    <p class="fs-20" style="font-weight: 300; line-height: 24px">
+      냉장고에 넣어둔 찌개를 일반 그릇에다 넣고<br />
+      데웠더니 전자레인지 안에서 터졌어요!<br />
+      뚜껑이 있는 용기가 필요해요.
+    </p>
+    <hr class="price-tag-line" />
+    <h4 class="fs-32" style="font-weight: 500">₩14,900</h4>
   </div>
 </template>
 
@@ -20,7 +40,29 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
+}
+.wish-background {
+  width: 412px;
+  height: 917px;
+  background: white;
+  outline: solid 1px black;
+}
+.present-state-line {
+  width: 377px;
+  border-bottom: 1px solid black;
+}
+.price-tag-line {
+  width: 187px;
+  border-bottom: 1px solid black;
+}
+.fs-36 {
+  font-size: 36px;
+}
+.fs-32 {
+  font-size: 32px;
+}
+.fs-20 {
+  font-size: 20px;
 }
 </style>
