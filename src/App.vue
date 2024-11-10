ea436379ed73e14a520bf464a12a03b210ae46e0
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-11-11 01:53:56 +0900

    feat: WishList.vue component App.vue에 연결하기

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,18 @@
 <template>
-  <div class="wish-background">
-    <div>
-      <h3
-        class="fs-36 fw-500"
-        style="text-align: left; line-height: 43px">
-        누군가가 이미<br />이 선물을 보냈어요!
-      </h3>
-      <hr class="present-state-line" />
-    </div>
-    <h3 class="fs-36 fw-500">
-      글라스락 렌지쿡
-    </h3>
-    <span class="fs-20 fw-500">탕, 찌개용</span
-    >
-    <img src="./assets/test.png" />
-    <p class="fs-20 fw-300">
-      냉장고에 넣어둔 찌개를 일반 그릇에다 넣고<br />
-      데웠더니 전자레인지 안에서 터졌어요!<br />
-      뚜껑이 있는 용기가 필요해요.
-    </p>
-    <hr class="price-tag-line" />
-    <h4 class="fs-32 fw-500">₩14,900</h4>
-  </div>
+<WishList></WishList>
 </template>
 
 <script>
+import WishList from "./components/WishList.vue"
 export default { 
   name: "App",
-  components: {},
+  data() {
+    return {
+    }
+  },
+  components: {
+    WishList
+  },
 };
 </script>
 
@@ -40,34 +25,5 @@ export default {
   text-align: center;
   line-height: 120%;
   color: #000000;
-}
-.wish-background {
-  width: 412px;
-  height: 917px;
-  background: white;
-  outline: solid 1px black;
-}
-.present-state-line {
-  width: 377px;
-  border-bottom: 1px solid black;
-}
-.price-tag-line {
-  width: 187px;
-  border-bottom: 1px solid black;
-}
-.fs-36 {
-  font-size: 36px;
-}
-.fs-32 {
-  font-size: 32px;
-}
-.fs-20 {
-  font-size: 20px;
-}
-.fw-300 {
-  font-weight: 500;
-}
-.fw-500 {
-  font-weight: 500;
 }
 </style>
