6bf683af81c31f6de582f14a3e21515cb5d4e756
hunter-hwang-dev <hunter.hwang.dev@gmail.com>
2024-11-11 00:59:58 +0900

    refactor: 폰트 사이즈, 두께 css 태그 정리

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <template>
   <div class="wish-background">
-    <div style="padding: 14px">
+    <div>
       <h3
-        class="fs-36"
-        style="font-weight: 500; text-align: left; line-height: 43px"
-      >
+        class="fs-36 fw-500"
+        style="text-align: left; line-height: 43px">
         누군가가 이미<br />이 선물을 보냈어요!
       </h3>
       <hr class="present-state-line" />
     </div>
-    <h3 class="fs-36" style="font-weight: 500; line-height: 36px">
+    <h3 class="fs-36 fw-500">
       글라스락 렌지쿡
     </h3>
-    <span class="fs-20" style="font-weight: 500; line-height: 24px"
-      >탕, 찌개용</span
+    <span class="fs-20 fw-500">탕, 찌개용</span
     >
     <img src="./assets/test.png" />
-    <p class="fs-20" style="font-weight: 300; line-height: 24px">
+    <p class="fs-20 fw-300">
       냉장고에 넣어둔 찌개를 일반 그릇에다 넣고<br />
       데웠더니 전자레인지 안에서 터졌어요!<br />
       뚜껑이 있는 용기가 필요해요.
     </p>
     <hr class="price-tag-line" />
-    <h4 class="fs-32" style="font-weight: 500">₩14,900</h4>
+    <h4 class="fs-32 fw-500">₩14,900</h4>
   </div>
 </template>
 
 <script>
-export default {
+export default { 
   name: "App",
   components: {},
 };
@@ -40,6 +38,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  line-height: 120%;
   color: #000000;
 }
 .wish-background {
@@ -64,5 +63,11 @@ export default {
 }
 .fs-20 {
   font-size: 20px;
+}
+.fw-300 {
+  font-weight: 500;
+}
+.fw-500 {
+  font-weight: 500;
 }
 </style>
